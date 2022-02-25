dd20ab6473bb21ffc6411d2088bdee0341549c73
gerainter <gerainter@gmail.com>
2022-02-25 01:09:05 -0600

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@ import React from "react";
 import "./style.css";
 
 export default function App() {
+  let img1 = "https://img.icons8.com/stickers/100/000000/arm-bracer.png";
+  let img2 = "https://img.icons8.com/stickers/100/000000/socks.png";
+  let img3 = "https://img.icons8.com/stickers/100/000000/purse-interior.png";
+  let nombres = ["Vaso","Calcetin","Bolsa"]
   return (
     <div className="capaPrincipal"> 
       <div className="capaImagen">
-      <img src="https://img.icons8.com/stickers/100/000000/arm-bracer.png"/>
-        <div className="formatoTexto">Titulo 1</div>
+      <img src={img1}/>
+        <div className="formatoTexto">{nombres[0]}</div>
       </div>
       <div className="capaImagen">
-      <img src="https://img.icons8.com/stickers/100/000000/socks.png"/>
-        <div className="formatoTexto">Titulo 2</div>
+      <img src={img2}/>
+        <div className="formatoTexto">{nombres[1]}</div>
       </div> 
       <div className="capaImagen">
-      <img src="https://img.icons8.com/stickers/100/000000/purse-interior.png"/>
-        <div className="formatoTexto">Titulo 3</div>
+      <img src={img3}/>
+        <div className="formatoTexto">{nombres[2]}</div>
       </div>      
     </div>
   );
