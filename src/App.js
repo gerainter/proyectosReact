54e694cb4ecadd1ea69b90b94490c6f4aaa47f4b
gerainter <gerainter@gmail.com>
2022-02-25 00:53:18 -0600

    2 files modified

--- src/App.js
+++ src/App.js
@@ -3,8 +3,19 @@ import "./style.css";
 
 export default function App() {
   return (
-    <div>
-      
+    <div className="capaPrincipal"> 
+      <div className="capaImagen">
+      <img src="https://img.icons8.com/stickers/100/000000/arm-bracer.png"/>
+        <div className="formatoTexto">Titulo 1</div>
+      </div>
+      <div className="capaImagen">
+      <img src="https://img.icons8.com/stickers/100/000000/socks.png"/>
+        <div className="formatoTexto">Titulo 2</div>
+      </div> 
+      <div className="capaImagen">
+      <img src="https://img.icons8.com/stickers/100/000000/purse-interior.png"/>
+        <div className="formatoTexto">Titulo 3</div>
+      </div>      
     </div>
   );
 }
