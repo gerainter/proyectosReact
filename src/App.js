dec03260dd18943079a0b69e998dbfc8fffe9b78
gerainter <gerainter@gmail.com>
2022-02-26 22:39:09 -0600

    2 files modified

--- src/App.js
+++ src/App.js
@@ -7,26 +7,52 @@ const App = () => {
     setNombre("Hiren")
   }
 
-  const[numero,setNumero] = useState(0);
-
+  let[numero,setNumero] = useState(0);
   const incrementar=()=>{
-    setNumero(numero+1);
+    setNumero(numero++);
+  }
+  const decrementar=()=>{
+    setNumero(numero--);
+  }
+  const resetear= () =>{
+    setNumero(0);
   }
 
-  const decrementar=()=>{
-    setNumero(numero-1);
+  const [numero1,setNumero1] = useState();
+  const [numero2,setNumero2] = useState();
+  const [resultado,setResultado] = useState();
+
+  const suma=()=>{
+    setResultado(Number(numero1)+Number(numero2));
+  }
+
+  const actualiza1=(e)=>{
+    setNumero1(e.target.value);
+  }
+
+  const actualiza2=(e)=>{
+    setNumero2(e.target.value);
   }
 
   return (
+      <>
       <div>
           <h1>Hola me llamo {nombre}</h1>
           <button onClick={cambiarNombre}>Cambiar Nombre</button>
           <button onClick={()=>setNombre("Goku")}>Cambiar Nombre</button>
           <h1>Numero:{numero}</h1>
-          <button onClick={incrementar}>Incrementar</button>
+          <button onClick={()=>setNumero(numero++)}>Incrementar</button>
           <button onClick={decrementar}>Decrementar</button>
-          <button>Resetear</button>
+          <button onClick={resetear}>Resetear</button>
       </div>
+      <p></p>
+      <div className="caja">
+        <input type="number" value={numero1} onChange={actualiza1}/>+
+        <input type="number" value={numero2} onChange={actualiza2}/>=
+        <input type="number" value={resultado} readOnly/>
+        <button onClick={suma} className="btnSuma">Sumar</button>
+      </div>
+      </>
   );
 }
 
