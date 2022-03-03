faef097a9904643bc32a12b3009523d23c3be505
gerainter <gerainter@gmail.com>
2022-03-03 09:11:06 -0600

    3 files modified

--- src/App.js
+++ src/App.js
@@ -4,13 +4,45 @@ import React from "react";
 import Componente1 from "./Componente1"
 
 export default function App() {
- 
+  let imagen1= "https://html6.es/img/rey_atanagildo.png";
+  let imagen2= "https://html6.es/img/rey_ervigio.png";
+  let imagen3= "https://html6.es/img/rey_ataulfo.png";
+  let imagen4= "https://html6.es/img/rey_leogivildo.png";
+  const reyes=[
+    {
+      nombre:"Atanagildo",
+      color:"darkolivegreen",
+      precio:178
+    },{
+      nombre:"Ervigio",
+      color:"crimson",
+      precio:169
+    },{
+      nombre:"Ataúlfo",
+      color:"peru",
+      precio:81
+    },{
+      nombre:"Leogivildo",
+      color:"darkmagenta",
+      precio:126
+    },{
+      nombre:"Recesvinto",
+      color:"royalblue",
+      precio:141
+    },{
+      nombre:"Sisebuto",
+      color:"teal",
+      precio:69
+    }
+  ]
   return (
     <>
       <h1>Total a pagar:</h1>
       <div className="capaPrincipal">
-        <Componente1></Componente1>
-        <Componente1></Componente1>
+        <Componente1 imagen={imagen1} titulo="Titulo imagen 1" precio={123}></Componente1>
+        <Componente1 imagen={imagen2} titulo="Titulo imagen 2" precio={567}></Componente1>
+        <Componente1 imagen={imagen3} titulo="Titulo imagen 3" precio={123}></Componente1>
+        <Componente1 imagen={imagen4} titulo="Titulo imagen 4" precio={567}></Componente1>
       </div>
     </>
   );
--- src/Componente1.jsx
+++ src/Componente1.jsx
@@ -1,46 +1,20 @@
 import "./style.css";
-import React from "react";
+import React,{useState} from "react";
 
- function Componente1() {
- let imagen1= "https://html6.es/img/rey_atanagildo.png";
- const reyes=[
-  {
-    nombre:"Atanagildo",
-    color:"darkolivegreen",
-    precio:178
-  },{
-    nombre:"Ervigio",
-    color:"crimson",
-    precio:169
-  },{
-    nombre:"Ataúlfo",
-    color:"peru",
-    precio:81
-  },{
-    nombre:"Leogivildo",
-    color:"darkmagenta",
-    precio:126
-  },{
-    nombre:"Recesvinto",
-    color:"royalblue",
-    precio:141
-  },{
-    nombre:"Sisebuto",
-    color:"teal",
-    precio:69
-  }
-]
+ function Componente1(prop) {
+
+ 
   return (
     <>
       <div className="capaImagen">
-        <div className="capaTitulo">Titulo Imagen</div>
+        <div className="capaTitulo">{prop.titulo}</div>
         <div>
-          <img src={imagen1}/>
+          <img src={prop.imagen}/>
          </div>
          <div className="capaPrecio">
             <span>Precio:</span>
             <br></br>
-            <span>1223</span>
+            <span>{prop.precio}</span>
             <br></br>
             <button>Comprar</button>
          </div> 
